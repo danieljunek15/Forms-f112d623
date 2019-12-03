06de1e5c6a573d315c719a59079482e64b347609
danieljunek15 <dvandenbrink@roc-dev.com>
2019-12-03 11:51:40 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="styles.css">
+<title> Email box. </title>
+</head>
+<body>
+
+<form action="" methode="get" class="form-example">
+<div class="form-example">
+<label for="email">Enter your email:
+</label> 
+<input type="email" value="Voer hier nu je email in pipo!" id="email" 
+required>
+</div>
+<div class="form-example">
+<input type="submit" value="Verstuur pipo!">
+</div>
+</form>
+
+</body>
+</html>
